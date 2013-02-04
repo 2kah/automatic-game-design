59f8dc5be9baa936516bc4e9e9f69025586b92dd
adam.2kah@gmail.com <adam.2kah@gmail.com@8169ca83-ae87-0d2c-e5b3-1d1f84eb771b>
2013-02-04 15:41:12 +0000

    Added more comments

--- src/agd/gridgame/LongRandomController.java
+++ src/agd/gridgame/LongRandomController.java
@@ -6,11 +6,20 @@ package agd.gridgame;
  * Date: Jul 25, 2008
  * Time: 7:06:17 PM
  */
+
+/**
+ * A controller which selects a random direction to move in and a random number of steps to move in that direction
+ */
 public class LongRandomController implements Controller, Constants {
 
     private int stepsLeft = 0;
     private direction current = null;
 
+    /**
+     *
+     * @param state a game state (eg. the current state of the current game)
+     * @return a random action, which may be part of a sequence of planned actions to move in a particular direction
+     */
     public Action control(GameState.Description state) {
         if (stepsLeft < 1) {
             current = direction.values()[(int) (Math.random () * direction.values().length)];
--- src/agd/gridgame/RandomController.java
+++ src/agd/gridgame/RandomController.java
@@ -6,8 +6,17 @@ package agd.gridgame;
  * Date: Jul 24, 2008
  * Time: 10:28:01 PM
  */
+
+/**
+ * A controller which selects a random move every time the control() function is called
+ */
 public class RandomController implements Controller, Constants {
-    
+
+    /**
+     *
+     * @param state a game state (eg. the current state of the current game)
+     * @return a valid random action
+     */
     public Action control(GameState.Description state) {
         Action action = new Action ();
         action.movement = direction.values()[(int) (Math.random () * direction.values().length)];
