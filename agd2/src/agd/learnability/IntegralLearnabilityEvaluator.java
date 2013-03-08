c6c9e74d71a6801e8c13f8f7eb1497b8f7fff4d7
adam.2kah@gmail.com <adam.2kah@gmail.com@8169ca83-ae87-0d2c-e5b3-1d1f84eb771b>
2013-03-08 11:40:54 +0000

    Test code which generates learning curve data

--- agd2/src/agd/learnability/IntegralLearnabilityEvaluator.java
+++ agd2/src/agd/learnability/IntegralLearnabilityEvaluator.java
@@ -5,12 +5,6 @@ import agd.evolution.ControllerLearner;
 import agd.evolution.Evolvable;
 import agd.evolution.PlayerEvolver;
 import agd.gridgame.Parameters;
-import org.jfree.chart.ChartFactory;
-import org.jfree.chart.ChartFrame;
-import org.jfree.chart.JFreeChart;
-import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.data.xy.XYSeries;
-import org.jfree.data.xy.XYSeriesCollection;
 
 
 /**
@@ -25,24 +19,27 @@ public class IntegralLearnabilityEvaluator implements LearnabilityEvaluator {
     final int populationSize = 100;
     final int generations = 100;
 
+    public double[] fitnesses;
+
     public double[] evaluate(ControllerLearner learner, Parameters parameters) {
         // idea: divide the t-value (should be high) with the average deviation from the regressed straight line (should be low)
-        double[] fitnesses = new double[generations]; // best fitness of each generation
+        //double[] fitnesses = new double[generations]; // best fitness of each generation
+        fitnesses = new double[generations];
         double[] standardDeviations = new double[generations]; // sd of best individual of each generation
 
         // first, get the learning curve as usual
         runLearningAlgorithm (learner, parameters, fitnesses, standardDeviations);
 
         // plot learning curve
-        XYSeriesCollection dataset = new XYSeriesCollection();
-        XYSeries series = new XYSeries("learningCurve");
-        for (int i = 0; i < fitnesses.length; i++)
-            series.add(i, fitnesses[i]);
-        dataset.addSeries(series);
-        JFreeChart chart = ChartFactory.createScatterPlot("Learning curve", "X", "Y", dataset, PlotOrientation.VERTICAL, true, true, false);
-        ChartFrame frame = new ChartFrame("First", chart);
-        frame.pack();
-        frame.setVisible(true);
+//        XYSeriesCollection dataset = new XYSeriesCollection();
+//        XYSeries series = new XYSeries("learningCurve");
+//        for (int i = 0; i < fitnesses.length; i++)
+//            series.add(i, fitnesses[i]);
+//        dataset.addSeries(series);
+//        JFreeChart chart = ChartFactory.createScatterPlot("Learning curve", "X", "Y", dataset, PlotOrientation.VERTICAL, true, true, false);
+//        ChartFrame frame = new ChartFrame("First", chart);
+//        frame.pack();
+//        frame.setVisible(true);
 
 
         // then, do linear regression on this (for now, just fit the first and last?)
@@ -69,7 +66,7 @@ public class IntegralLearnabilityEvaluator implements LearnabilityEvaluator {
 
     private void runLearningAlgorithm (ControllerLearner learner, Parameters parameters,
                                        double[] fitnesses, double[] standardDeviations) {
-        System.out.println("Running learning algorithm...");
+        //System.out.println("Running learning algorithm...");
 
         // learner has already been initialised
 //        Controller initial = new RMLPControllerPlus();
@@ -81,8 +78,7 @@ public class IntegralLearnabilityEvaluator implements LearnabilityEvaluator {
             learner.oneMoreGeneration ();
             fitnesses[generation] = learner.getBestFitness();
             standardDeviations[generation] = learner.getBestFitnessSD();
-            System.out.println("Generation " + generation + " fitness " + fitnesses[generation] +
-                    " sd " + standardDeviations[generation]);
+            //System.out.println("Generation " + generation + " fitness " + fitnesses[generation] + " sd " + standardDeviations[generation]);
         }
     }
 
--- agd2/src/agd/learnability/Test.java
+++ agd2/src/agd/learnability/Test.java
@@ -1,9 +1,13 @@
 package agd.learnability;
 
+import agd.controllers.*;
+import agd.evolution.ControllerLearner;
+import agd.evolution.Evolvable;
+import agd.evolution.PlayerEvolver;
 import agd.gridgame.Parameters;
 import wox.serial.Easy;
-import agd.evolution.*;
-import agd.controllers.*;
+
+import java.io.FileWriter;
 
 /**
  * Created by IntelliJ IDEA.
@@ -24,17 +28,77 @@ public class Test {
         }
         System.out.println(parameters);
 
-        LearnabilityEvaluator learnabilityEval = null;
-        learnabilityEval = new IntegralLearnabilityEvaluator();
+        //LearnabilityEvaluator learnabilityEval = new IntegralLearnabilityEvaluator();
         //learnability = new HighscoreLearnabilityEvaluator();
-        Evolvable controller = new RMLPControllerPlus();
-        ControllerLearner  controllerEvolver = null;
-        controllerEvolver = new PlayerEvolver(parameters, controller);
+        //Evolvable controller = new RMLPControllerPlus();
+        //ControllerLearner controllerEvolver = null;
+
+        //int generations = 100;
+        //controllerEvolver = new PlayerEvolver(parameters, controller, generations, 20);
         //learner = new RandomPlayerSearch(parameters, controller);
 
-        double[] results = learnabilityEval.evaluate(controllerEvolver, parameters);
-        System.out.println("Learnability: " + results[0]);
+        //double[] results = learnabilityEval.evaluate(controllerEvolver, parameters);
+        //System.out.println("Learnability: " + results[0]);
 
+        //IntegralLearnabilityEvaluator learnability = (IntegralLearnabilityEvaluator) learnabilityEval;
+
+        generateCurve(parameters, new MLPController(), "MLPController");
+        generateCurve(parameters, new MLPControllerPlus(), "MLPControllerPlus");
+        //generateCurve(parameters, new PriorityController(), "PriorityController");
+        //generateCurve(parameters, new PriorityControllerHP(), "PriorityControllerHP");
+        //generateCurve(parameters, new PriorityControllerTLB(), "PriorityControllerTLB");
+        generateCurve(parameters, new RMLPController(), "RMLPController");
+        generateCurve(parameters, new RMLPControllerPlus(), "RMLPControllerPlus");
+
+    }
+
+    private static void generateCurve(Parameters parameters, Evolvable controllerType, String controllerName)
+    {
+        int generations = 100;
+        double[] learningCurve = new double[generations];
+        for(int i = 0; i < learningCurve.length; i++)
+            learningCurve[i] = 0;
+
+        int curvesToAverage = 100;
+
+        for(int i = 0; i < curvesToAverage; i++)
+        {
+            Evolvable controller = controllerType.newInstance();
+            ControllerLearner controllerEvolver = new PlayerEvolver(parameters, controller, generations, 20);
+            IntegralLearnabilityEvaluator learnability = new IntegralLearnabilityEvaluator();
+
+            learnability.evaluate(controllerEvolver, parameters);
+            System.out.println("Evaluation " + i + " complete for " + controllerName);
+            double[] tempCurve = learnability.fitnesses;
+            for(int j = 0; j < learningCurve.length; j++)
+            {
+                learningCurve[j] += tempCurve[j];
+            }
+        }
+
+        for(int i = 0; i < learningCurve.length; i++)
+            learningCurve[i] /= curvesToAverage;
+
+        writeToCsv(controllerName + ".csv", learningCurve);
+    }
+
+    private static void writeToCsv(String filename, double[] values)
+    {
+        try
+        {
+            FileWriter writer = new FileWriter(filename);
+
+            for(int i = 0; i < values.length; i++)
+            {
+                writer.append(Double.toString(values[i]) + '\n');
+            }
+            writer.flush();
+            writer.close();
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 
 
