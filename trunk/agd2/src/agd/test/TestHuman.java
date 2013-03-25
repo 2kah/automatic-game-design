f369f25d8c4ffce97c5a84beeb16b1b8ae728915
adam.2kah@gmail.com <adam.2kah@gmail.com@8169ca83-ae87-0d2c-e5b3-1d1f84eb771b>
2013-03-25 16:16:34 +0000

    Adapted Play.java so it can be used for human testing

--- trunk/agd2/src/agd/test/TestHuman.java
+++ trunk/agd2/src/agd/test/TestHuman.java
@@ -1,5 +1,15 @@
 package agd.test;
 
+import agd.gridgame.Controller;
+import agd.gridgame.KeyboardController;
+import agd.gridgame.Parameters;
+import agd.gridgame.Play;
+import wox.serial.Easy;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.Arrays;
+
 /**
  * Created with IntelliJ IDEA.
  * User: 2kah
@@ -9,4 +19,31 @@ package agd.test;
  */
 public class TestHuman {
 
+    public static void main(String[] args) throws Exception
+    {
+        Parameters parameters = null;
+        String paramName = "";
+        paramName = args[0];
+        try {
+            parameters = (Parameters) Class.forName (paramName).newInstance ();
+        }
+        catch (ClassNotFoundException e) {
+            System.out.println (paramName + " is not a class name; trying to load a wox definition with that name.");
+            parameters = (Parameters) Easy.load(paramName);
+        }
+        System.out.println(parameters);
+        Controller controller = new KeyboardController();
+        if (args.length > 1) {
+            try {
+                controller = (Controller) Class.forName (args[1]).newInstance ();
+            }
+            catch (ClassNotFoundException e) {
+                System.out.println (args[1] + " is not a class name; trying to load a wox definition with that name.");
+                parameters = (Parameters) Easy.load (args[1]);
+            }
+        }
+        System.out.println(controller);
+        // Play the game and output results
+        Play.play (parameters, controller, args[0]);
+    }
 }
