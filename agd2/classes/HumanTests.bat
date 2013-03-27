1dddcea88bf4b03744ab2d39661c3e8721e81f6d
adam.2kah@gmail.com <adam.2kah@gmail.com@8169ca83-ae87-0d2c-e5b3-1d1f84eb771b>
2013-03-27 21:30:48 +0000

    Batch file for running human tests

---
+++ agd2/classes/HumanTests.bat
@@ -0,0 +1,17 @@
+@echo off
+
+echo Game 1
+java agd.test.TestHuman agd.gridgame.games.Casino
+echo Game 2
+java agd.test.TestHuman agd.gridgame.games.Golfer
+echo Game 3
+java agd.test.TestHuman agd.gridgame.games.Hyenas
+echo Game 4
+java agd.test.TestHuman agd.gridgame.games.HyenasPro
+echo Game 5
+java agd.test.TestHuman agd.gridgame.games.Original
+echo Game 6
+java agd.test.TestHuman agd.gridgame.games.Terrorists
+echo Game 7
+java agd.test.TestHuman agd.gridgame.games.TrafficLightBuffet
+echo Thank you for taking part in this experiment
