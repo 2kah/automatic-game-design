7610519f15c6c6b8747fdd1b0d6bd249b645022f
adam.2kah@gmail.com <adam.2kah@gmail.com@8169ca83-ae87-0d2c-e5b3-1d1f84eb771b>
2013-04-08 15:44:02 +0000

    Template class for new ParameterEvaluator

---
+++ agd2/src/agd/evolution/LearningCurveParameterEvaluator.java
@@ -0,0 +1,19 @@
+package agd.evolution;
+
+import agd.gridgame.Parameters;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: 2kah
+ * Date: 04/04/13
+ * Time: 20:39
+ * To change this template use File | Settings | File Templates.
+ */
+public class LearningCurveParameterEvaluator implements ParameterEvaluator
+{
+    public double[] evaluate(Parameters parameters)
+    {
+        //
+        return new double[0];
+    }
+}
